a5f3c50f86aa16ca5d2d7907b3d4cd32f56a3ad2
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2024-04-11 14:32:14 +0530

    Added speed-insights from vercel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Poppins } from 'next/font/google'
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Poppins({ subsets: ["latin"],weight:['400'] });
 
@@ -11,12 +11,13 @@ export const metadata = {
 
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights/>
+        </body>
     </html>
   );
 }
